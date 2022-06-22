0a47faa45c4be2bab04babd0f3e3aa401f7d5bfd
alimh17 <amohamadi17@gamil.com>
2022-06-22 16:27:11 +0430

    end light version

--- apps/splid.js
+++
@@ -1,9 +0,0 @@
-// Default theme
-import '@splidejs/splide/css';
-// or other themes
-import '@splidejs/splide/css/skyblue';
-import '@splidejs/splide/css/sea-green';
-// or only core styles
-import '@splidejs/splide/css/core';
-import Splide from '@splidejs/splide';
-
